32e6cf3b31364e096cae031f25dfa0e22b5810c9
Bardia Khosravi <bardiakhosravi95@gmail.com>
2024-05-19 10:17:56 -0400

    moding to flint

--- radprompter/__init__.py
+++ radprompter/__init__.py
@@ -1,3 +1,7 @@
+"""A package for simplified and reproducible LLM prompting."""
+
 from .radprompter import RadPrompter
 from .prompts import Prompt
 from .clients import OpenAIClient, vLLMClient, OllamaClient
+
+__version__ = "1.0"
